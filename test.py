f072351864ee7ea652d60638e93fe643994434cf
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-06-29 18:45:07 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+from tensorflow.keras.datasets import  mnist
+from tensorflow.keras.models import  load_model
+
+classifier = load_model('C:\\Users\\lukas.rimkus\\Software\\DLCVudm\\mnist.h5')
+
+
